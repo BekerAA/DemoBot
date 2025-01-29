8bc6dfe78b2b68bbf230e4451b3408ae39a3940b
Andrey <andrey.pushkov.99@bk.ru>
2025-01-29 13:57:44 +0300

    Create project

---
+++ src/main/java/ru/Andrey/App.java
@@ -0,0 +1,13 @@
+package ru.Andrey;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
