88eee77e8beefd5c4d7561f96902237899b72d75
Andrey <andrey.pushkov.99@bk.ru>
2025-03-01 15:38:04 +0300

    RMQ connection

--- dispatcher/src/main/java/ru/Andrey/service/Impl/UpdateProducerImpl.java
+++ dispatcher/src/main/java/ru/Andrey/service/Impl/UpdateProducerImpl.java
@@ -1,6 +1,7 @@
 package ru.Andrey.service.Impl;
 
 import lombok.extern.log4j.Log4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import ru.Andrey.service.UpdateProducer;
@@ -9,9 +10,17 @@ import ru.Andrey.service.UpdateProducer;
 @Service
 @Log4j
 public class UpdateProducerImpl implements UpdateProducer {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public UpdateProducerImpl(RabbitTemplate rabbitTemplate){
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
     @Override
     public void produce(String rabbitQueue, Update update) {
         log.debug(update.getMessage().getText());//На данный момент это заглушка
+        rabbitTemplate.convertAndSend(rabbitQueue,update);
 
     }
 }
